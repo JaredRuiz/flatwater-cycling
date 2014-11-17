774da2f0285d8b85b9a995ba00506e6f14e925f8
JaredRuiz <jared@semcat.net>
2014-11-17 13:19:36 -0600

    adds style to the line

--- one-line-to-two.js
+++ one-line-to-two.js
@@ -2,9 +2,7 @@
 function MapLine(linePoints, speedPoints) {
   this.mainLine = []; //arr.map(function(a) { return a; });
   for (var j = 0; j < linePoints.length; j++) {
-    // if (j % 10 == 0) {
-      this.mainLine.push(linePoints[j]);
-    // }
+    this.mainLine.push(linePoints[j]);
   } // for
 
   this.speedPoints = [];
@@ -21,10 +19,10 @@ function MapLine(linePoints, speedPoints) {
 }; // MapLine
 
 MapLine.prototype.makeRad = function(num) {
-    num = parseFloat(num).toFixed(4);
-    num = (num === "-0.0000") ? "0.0000" : num;
-    return parseFloat(num);
-  } // makeRad
+  num = parseFloat(num).toFixed(4);
+  num = (num === "-0.0000") ? "0.0000" : num;
+  return parseFloat(num);
+} // makeRad
 
 // TODO it would be good to pull these out into another class...
 MapLine.prototype.cos = function(a) {
@@ -32,16 +30,16 @@ MapLine.prototype.cos = function(a) {
 } // cos
 
 MapLine.prototype.acos = function(a) {
-var tempRad = this.makeRad(Math.acos(a));
+  var tempRad = this.makeRad(Math.acos(a));
   if (tempRad >= 2*this.pi) {
-      tempRad - 2*this.pi;
+    tempRad - 2*this.pi;
     return this.acos(tempRad);
-} else if (tempRad < 0) {
-  tempRad + 2*this.pi;
-  return this.acos(tempRad);
-} else {
-  return tempRad;
-}
+  } else if (tempRad < 0) {
+    tempRad + 2*this.pi;
+    return this.acos(tempRad);
+  } else {
+    return tempRad;
+  }
 } // acos
 
 MapLine.prototype.sin = function(a) {
@@ -55,9 +53,6 @@ MapLine.prototype.asin = function(a) {
 MapLine.prototype.pi = function() { return this.makeRad(Math.PI) }
 
 MapLine.prototype.makeOnePointTwo = function(point1, point2, speedPoint) {
-
-  // console.log(speedPoint);
-
   // helper functions
   var makeRad = function(num) { return parseFloat(num).toFixed(5); }
   var _c = function(p1, p2, idx) { return p2[idx] - p1[idx]; }
@@ -68,14 +63,19 @@ MapLine.prototype.makeOnePointTwo = function(point1, point2, speedPoint) {
   }
   // take difference betwen points in order to shift to origin
   var diffPoint = [_xc(point1, point2), _yc(point1, point2)];
-  
+
+  /////////////////////////////////////////////////////
+  // main
+
+  // TODO: comment throughout on what step in the process you're in..
+
   // compute distance of diffPoint to origin (ie. length of diffPoint as a vector)
   var rad = dist([0, 0], diffPoint);
 
   // var ang = rad/diffPoint[0];
   var ang = diffPoint[0]/rad;
 
-  // console.log("agnle is: " + ang);
+
 
   /// Since x=rCos(a) and y=rSin(a) (for an angle a) and we know x, y, and r, we compute theta
   // TODO fix acos...its broken
@@ -88,37 +88,18 @@ MapLine.prototype.makeOnePointTwo = function(point1, point2, speedPoint) {
   var aboveAngle = parseFloat((theta + this.pi()/2));
   var belowAngle = parseFloat((theta - this.pi()/2));
 
-
+  var scale = speedPoint * .00001;
   
-  // var scale = parseFloat(".00" + speedPoint.toString());
-  var scale = speedPoint * .0001;
-  // var scale = parseFloat(speedPoint * .1);
-  // var scale = parseFloat(speedPoint);
-
-  // console.log(scale);
-
-  
-
   /// Compute points on this new line made by above angles, at distance 1 from origin 
   var tempAbovePoint = [scale*this.cos(aboveAngle), scale*this.sin(aboveAngle)];
   var tempBelowPoint = [scale*this.cos(belowAngle), scale*this.sin(belowAngle)];
-
-  // var tempAbovePoint = [this.cos(aboveAngle), this.sin(aboveAngle)];
-  // var tempBelowPoint = [this.cos(belowAngle), this.sin(belowAngle)];
-
-  /// Shift these new points back to where point1 and point2 initially began
-  //TODO  this is wrong...
-  // var abovePoint1 = [point1[0] + tempAbovePoint[0] -.1, point1[1] + tempAbovePoint[1]];
-  // var abovePoint2 = [point2[0] + tempAbovePoint[0]-.1, point2[1] + tempAbovePoint[1]];
-  // var belowPoint1 = [point1[0] + tempBelowPoint[0]-.1, point1[1] + tempBelowPoint[1]];
-  // var belowPoint2 = [point2[0] + tempBelowPoint[0]-.1, point2[1] + tempBelowPoint[1]];
 
   var abovePoint1 = [point1[0] + tempAbovePoint[0], point1[1] + tempAbovePoint[1]];
   var abovePoint2 = [point2[0] + tempAbovePoint[0], point2[1] + tempAbovePoint[1]];
   var belowPoint1 = [point1[0] + tempBelowPoint[0], point1[1] + tempBelowPoint[1]];
   var belowPoint2 = [point2[0] + tempBelowPoint[0], point2[1] + tempBelowPoint[1]];
 
-   var returnPoints = [
+  var returnPoints = [
     [abovePoint1, abovePoint2],
     [belowPoint1, belowPoint2]
   ];
@@ -129,8 +110,6 @@ MapLine.prototype.makeOnePointTwo = function(point1, point2, speedPoint) {
 MapLine.prototype.generateDoublePoints = function() {
   
   if (this.mainLine.length !== this.speedPoints.length) {
-    // console.log(this.mainLine.length);
-    // console.log(this.speedPoints.length);
     throw new Error("array lengths do not match");
   }
 
@@ -150,8 +129,7 @@ MapLine.prototype.generateDoublePoints = function() {
       if (newPoints[1][0].length > 0 && newPoints[1][1].length > 0) {
         doublePoints.below = doublePoints.below.concat(newPoints[1]);
       }
-      // console.log(doublePoints.above);
-    }
+     }
   } // for
   return doublePoints;
 }
@@ -161,11 +139,8 @@ MapLine.prototype.makePolys = function() {
   for (var j = 0; j < this.aboveLinePoints.length; j++) {
     if (j < this.aboveLinePoints.length-1) {
       polys.push([this.aboveLinePoints[j], this.aboveLinePoints[j+1],
-                          this.belowLinePoints[j+1], this.belowLinePoints[j]]);
+                  this.belowLinePoints[j+1], this.belowLinePoints[j]]);
     }
   }
   return polys;
 };
-
-
-// exports.MapLine = MapLine;
